70f009e27f7c2d8c3ec594f598d03181dbfedc28
tanota <tanota@student.42.fr>
2023-12-12 17:48:17 +0100

    ex00 done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,22 @@
 /*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 19:18:17 by tanota            #+#    #+#             */
-/*   Updated: 2023/11/23 19:57:24 by tanota           ###   ########.fr       */
+/*   Updated: 2023/12/12 17:40:31 by tanota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
