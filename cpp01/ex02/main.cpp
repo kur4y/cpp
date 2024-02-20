eadeb4caf75d34d1d780c9345083ba88ed81f165
tanota <tanota@student.42.fr>
2024-02-20 17:13:40 +0100

    ex02_done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 17:03:02 by tanota            #+#    #+#             */
+/*   Updated: 2024/02/20 17:07:16 by tanota           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << &str << ": " << str << std::endl;
+	std::cout << stringPTR << ": " << *stringPTR << std::endl;
+	std::cout << &stringREF << ": " << stringREF << std::endl;
+}
