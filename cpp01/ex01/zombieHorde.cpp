c89ecff6712e6e3d6cedbbf54bf5c12199f4e373
tanota <tanota@student.42.fr>
2024-02-20 17:01:14 +0100

    horde_updated

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:24:37 by tanota            #+#    #+#             */
-/*   Updated: 2024/02/19 18:29:11 by tanota           ###   ########.fr       */
+/*   Updated: 2024/02/20 16:59:49 by tanota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Zombie* zombieHorde(int N, std::string name)
 	Zombie* zHorde = new Zombie[N];
 	
 	for (int i = 0; i < N; i++)
-		zHorde[N].setName(name);
+		zHorde[i].setName(name);
 	
 	return zHorde;
 }
