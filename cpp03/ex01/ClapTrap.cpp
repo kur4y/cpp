0e3258fae11eb277c242117d3d186c7cd4ad3819
tanota <tanota@student.42.fr>
2024-03-03 00:21:13 +0100

    add_ex01

---
+++ cpp03/ex01/ClapTrap.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 15:02:10 by tanota            #+#    #+#             */
+/*   Updated: 2024/03/03 00:12:06 by tanota           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+				: _name(""), _hitPoints(100), _energyPoints(50), _attackDamage(20)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap " << _name << " has been destroyed." << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string& name)
+				: _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap " << name << " has been created." << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+				: _name(other._name), _hitPoints(other._hitPoints), _energyPoints(other._energyPoints), _attackDamage(other._attackDamage)
+{
+	std::cout << "ClapTrap " << _name << " has been copy-constructed." << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+	if (this != &other)
+	{
+		_name = other._name;
+		_hitPoints = other._hitPoints;
+		_energyPoints = other._energyPoints;
+		_attackDamage = other._attackDamage;
+	}
+	std::cout << "ClapTrap " << _name << " has been assigned." << std::endl;
+	return *this;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (_energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << _name << " has not enough energy points. Can't attack." << std::endl;
+		return ;
+	}
+	else if (_hitPoints == 0)
+		std::cout << "ClapTrap " << _name << " has not enough hit points. Can't attack." << std::endl;
+	_energyPoints--;
+	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl; 
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hitPoints <= amount)
+	{
+		_hitPoints = 0;
+		std::cout << "ClapTrap " << _name << " took " << amount << " points of damage. " << _name << " has been destroyed." << std::endl;
+	}
+	else
+	{
+		_hitPoints -= amount;
+		std::cout << "ClapTrap " << _name << " took " << amount << " points of damage. " << _name << " has now " << _hitPoints << " hit points left." << std::endl;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << _name << " is out of energy. " << _name << " can't be repaired." << std::endl;
+		return ;
+	}
+	_energyPoints--;
+	_hitPoints += amount;
+	std::cout << "ClapTrap " << _name << " repaired for " << amount << " hit points. " << _name << " has now " << _hitPoints << "hit points." << std::endl;
+}
---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 15:02:15 by tanota            #+#    #+#             */
+/*   Updated: 2024/03/03 00:08:09 by tanota           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+    ClapTrap cp("John");
+    cp.attack("Nhoj");
+    cp.takeDamage(5);
+    cp.beRepaired(3);
+
+    ClapTrap cp2 = cp;
+    cp2.attack("David");
+    cp2.takeDamage(5);
+    cp2.beRepaired(3);
+
+    ClapTrap cp3;
+    cp3.attack("Tom");
+    cp3.takeDamage(5);
+    cp3.beRepaired(3);
+
+    return (0);
+}
