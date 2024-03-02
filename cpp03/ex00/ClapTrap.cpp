889b02dffd0ff4c2674ef2d0e6940128047cabc4
tanota <tanota@student.42.fr>
2024-03-03 00:05:57 +0100

    ex00_done

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:02:10 by tanota            #+#    #+#             */
-/*   Updated: 2024/03/02 15:40:22 by tanota           ###   ########.fr       */
+/*   Updated: 2024/03/03 00:01:30 by tanota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,38 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other)
 void ClapTrap::attack(const std::string& target)
 {
 	if (_energyPoints == 0)
+	{
 		std::cout << "ClapTrap " << _name << " has not enough energy points. Can't attack." << std::endl;
+		return ;
+	}
 	else if (_hitPoints == 0)
 		std::cout << "ClapTrap " << _name << " has not enough hit points. Can't attack." << std::endl;
 	_energyPoints--;
 	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl; 
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hitPoints <= amount)
+	{
+		_hitPoints = 0;
+		std::cout << "ClapTrap " << _name << " took " << amount << " points of damage. " << _name << " has been destroyed." << std::endl;
+	}
+	else
+	{
+		_hitPoints -= amount;
+		std::cout << "ClapTrap " << _name << " took " << amount << " points of damage. " << _name << " has now " << _hitPoints << " hit points left." << std::endl;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << _name << " is out of energy. " << _name << " can't be repaired." << std::endl;
+		return ;
+	}
+	_energyPoints--;
+	_hitPoints += amount;
+	std::cout << "ClapTrap " << _name << " repaired for " << amount << " hit points. " << _name << " has now " << _hitPoints << "hit points." << std::endl;
 }
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,13 +6,27 @@
 /*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:02:15 by tanota            #+#    #+#             */
-/*   Updated: 2024/03/02 15:02:38 by tanota           ###   ########.fr       */
+/*   Updated: 2024/03/03 00:05:16 by tanota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int main()
-{
-	return (0);
+int main() {
+    ClapTrap cp("John");
+    cp.attack("Nhoj");
+    cp.takeDamage(5);
+    cp.beRepaired(3);
+
+    ClapTrap cp2 = cp;
+    cp2.attack("David");
+    cp2.takeDamage(5);
+    cp2.beRepaired(3);
+
+    ClapTrap cp3;
+    cp3.attack("Tom");
+    cp3.takeDamage(5);
+    cp3.beRepaired(3);
+
+    return (0);
 }
