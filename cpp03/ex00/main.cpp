87ec579e898697da39176f3aa6b207ff17ee5e11
tanota <tanota@student.42.fr>
2024-03-03 00:08:34 +0100

    main_updated

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,13 +6,14 @@
 /*   By: tanota <tanota@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:02:15 by tanota            #+#    #+#             */
-/*   Updated: 2024/03/03 00:05:16 by tanota           ###   ########.fr       */
+/*   Updated: 2024/03/03 00:08:09 by tanota           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int main() {
+int main()
+{
     ClapTrap cp("John");
     cp.attack("Nhoj");
     cp.takeDamage(5);
